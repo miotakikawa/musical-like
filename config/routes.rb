0be03052725863ad3cfbaa360dd41d46f7a998cf
miotakikawa <m.n.baton1223@gmail.com>
2022-04-02 11:24:00 +0000

    admin_musical

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'musicals/index'
+    get 'musicals/show'
+    get 'musicals/new'
+    get 'musicals/edit'
+  end
+  devise_for :admins, controllers: {
+  sessions: 'admins/sessions'
+  }
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20220402111622_create_musicals.rb
@@ -0,0 +1,12 @@
+class CreateMusicals < ActiveRecord::Migration[5.2]
+  def change
+    create_table :musicals do |t|
+      t.string :title
+      t.string :image_id
+      t.text :introduction
+      t.integer :genre_id
+
+      t.timestamps
+    end
+  end
+end
