13400696cd3cecea9339ecdc2db0d603623d2489
miotakikawa <m.n.baton1223@gmail.com>
2022-04-07 02:31:55 +0000

    admin_musical_new

--- app/controllers/admin/musicals_controller.rb
+++ app/controllers/admin/musicals_controller.rb
@@ -1,13 +1,37 @@
 class Admin::MusicalsController < ApplicationController
   def index
-  end
-
-  def show
+   @musicals=Musical.all
   end
 
   def new
+   @musical=Musical.new
+  end
+
+  def create
+   @musical=Musical.new(item_params)
+   if @musical.save
+    redirect_to admin_musical_path(@musical.id)
+   else
+    render :new
+   end
+  end
+
+  def show
+   @musical = Musical.find(params[:id])
   end
 
   def edit
+   @musical = Musical.find(params[:id])
+  end
+
+  def update
+   @musical = Musicak.find(params[:id])
+   @musical.update(musical_params)
+   redirect_to admin_musicals_path
+  end
+
+  private
+  def musical_params
+    params.require(:musical).permit(:name, :image, :introduction)
   end
 end
