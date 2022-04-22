b036a186f45d970f82aa502ab2abd05fb86dd37d
miotakikawa <m.n.baton1223@gmail.com>
2022-04-22 15:04:34 +0000

    finish

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '4795d98897224a5d51c7954d178e200d0e244c2a2219b74e16bf4adcade1f7ba5ec2c443aeff81c94747276b50642d3b191b6f19b5bc94fe28c8fb1264770882'
